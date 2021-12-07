3ba4ac9da8a005a7d19ca6e0d087e2daa51457f2
KRom-i <emailroma@mail.ru>
2021-12-07 19:23:11 +0300

    HW_1

--- Lesson_1/src/main/java/Main.java
+++ Lesson_1/src/main/java/Main.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Main {
+import server.WebServer;
+
+public class Main {
+
+    public static void main (String[] args) {
+
+        new WebServer (5000).start ();
+    }
 }
